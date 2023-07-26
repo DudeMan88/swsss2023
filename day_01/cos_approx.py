ab46181114e159c15220cd5b4c6634d5a603efbd
Ben <osler.benjamin@gmail.com>
2023-07-26 09:29:06 -0400

    Finished the exercise

--- day_01/cos_approx.py
+++ day_01/cos_approx.py
@@ -3,18 +3,29 @@
 
 cosine approximation function
 """
-__author__ = 'Qusai Al Shidi'
-__email__ = 'qusai@umich.edu'
+__author__ = 'Ben Osler'
+__email__ = 'osler.benjamin@gmail.com'
 
 from math import factorial
 from math import pi
 
 
-def cos_approx(x, accuracy=10):
-    """
-    """
-    
-    return 
+def cos_approx(x, accuracy=10,lst=0):
+    ### lst is used to determine whether you want to do the Cosine Approximation using list comprehension or using a loop, (lst = 0 defaults to using a loop) ###
+    add = []
+    if lst == 1:
+        add = [(((-1)**n)/factorial((2*n)))*(x**(2*n)) for n in range(accuracy)]
+    else:
+        for n in range(accuracy): 
+            temp = (-1)**n
+            temp /= factorial(2*n)
+            temp *= (x**(2*n))
+            add.append(temp)
+        
+        
+        #add.append((((-1)**n)/factorial((2*n)))*(x**(2*n)))
+        
+    return sum(add)
 
 
 
